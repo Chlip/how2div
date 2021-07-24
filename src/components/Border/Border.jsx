7934911ccdc0b49c55996c64c858b4ce0b7b29b5
Chlip <bialas.maciej@gmail.com>
2021-07-24 03:22:32 +0200

    ;)

--- src/components/Border/Border.jsx
+++ src/components/Border/Border.jsx
@@ -7,6 +7,7 @@ import DivContext from '../../context/context'
 function Border() {
     const [border, setborder] = useState(["border",10,"none",[0,0,0,1]])
     const [result, setresult] = useState({})
+    const [prev, setprev] = useState("")
     const borderctx = useContext(BorderContext)
     const divctx = useContext(DivContext)
     
@@ -14,17 +15,25 @@ function Border() {
     useEffect(() => {
         
         if (divctx.div.find(e=>border[0] in e)) {
-            let newdiv = divctx.div.filter(e=>{
-                return e[border[0]] !== "border"
+            let newdiv = divctx.div.map(e=>{
+                if(prev in e){
+                    e[border[0]] = result
+                    delete e[prev]
+                }
+                return e
             })
-            console.log(newdiv)
+            
             divctx.setdiv(prev=>newdiv)
+            
 
         }
         
         else {
             divctx.setdiv(prev => [...prev, result])
+            
+            
         }
+        setprev(prev=>border[0])
         return () => {
             
         }
